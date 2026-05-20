f6d21cb73276ec33e05fa260b8884e4b4e03d3fc
miaudona <donanferyt@gmail.com>
2026-05-20 12:59:23 +0200

    Componetizar experiencia + Mejorar estructura de carpetas + Poner animacion de fondo

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				'nebula': 'nebula 15s ease-in-out infinite',
+			},
+			keyframes: {
+				nebula: {
+					'0%, 100%': { transform: 'translate(-50%, -50%) scale(1)' },
+					'25%': { transform: 'translate(0%, -80%) scale(1.5)' },
+					'50%': { transform: 'translate(-100%, -20%) scale(0.7)' },
+					'75%': { transform: 'translate(-20%, 30%) scale(1.4)' },
+				}
+			}
+		},
 	},
 	plugins: [],
 }
